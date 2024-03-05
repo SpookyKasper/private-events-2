cacfd5be39dfaa3d5462abaa77a9067c030bdef9
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-03-05 12:40:34 +0100

    Implement event creation and display

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,32 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!
+
   def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @user = current_user
+    @event = @user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: 422 #unprocessable_entity
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :location, :event_date)
   end
 end
