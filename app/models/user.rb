cda20290affa76e94ab7181acee0d7eeb11bf40d
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-03-05 12:07:03 +0100

    Associate user and event model + add devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :created_events, class_name: 'Event', inverse_of: 'creator'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
