0af137db79dc682eaff72a5525f50156e98639ce
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-03-06 11:00:26 +0100

    Add canceling participation option

--- app/controllers/attendings_controller.rb
+++ app/controllers/attendings_controller.rb
@@ -11,10 +11,22 @@ class AttendingsController < ApplicationController
 
     if @attending.save
       flash[:notice] = "You now participate in #{@event.title} ;)"
-      redirect_to events_path
+      redirect_to @event
     else
       flash[:alert] = 'Ooops check the participation again'
       render :new, status: 422
+    end
+  end
+
+  def destroy
+    @user = current_user
+    @event = Event.find(params[:id])
+    @attending = Attending.find_by(attended_event_id: @event.id, event_attendee_id: @user.id)
+    if @attending.destroy
+      flash[:notice] = "You successfuly canceled your participation to #{@event.title} ;)"
+      redirect_to @event
+    else
+      flash[:alert] = 'Event deletion went wrong :/'
     end
   end
 end
